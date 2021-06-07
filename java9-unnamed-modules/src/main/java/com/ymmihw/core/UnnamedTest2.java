c8d40cfba029c04a39064e82591f51a718ef2e67
wanghaoming <wanghaoming@ctfo.com>
2021-06-07 14:24:39 +0800

    add UnnamedTest2

---
+++ java9-unnamed-modules/src/main/java/com/ymmihw/core/UnnamedTest2.java
@@ -0,0 +1,12 @@
+package com.ymmihw.core;
+
+import java.util.logging.Logger;
+
+public class UnnamedTest2 {
+    private static final Logger log = Logger.getLogger(UnnamedTest2.class.getName());
+
+    public static void main(String... st) {
+        Module module = UnnamedTest2.class.getModule();
+        log.info(module.toString());
+    }
+}
